555ff4f77669d6be8a7140ea2559462023c64eb5
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2025-06-14 19:27:00 -0300

    Corrijo consulta de talles: sumo cantidades y agrupo correctamente

--- app.py
+++ app.py
@@ -46,28 +46,24 @@ def productos():
     productos = Producto.query.all()
     return render_template('productos.html', productos=productos)
 
-@app.route('/producto/<slug>')
-def producto_detalle(slug):
-    producto = Producto.query.filter_by(slug=slug).first()
-    if not producto:
-        return "Producto no encontrado", 404
+from sqlalchemy import func
 
-    imagenes = producto.imagenes.split(',') if producto.imagenes else []
-
-    raw_talles = db.session.query(
+raw_talles = (
+    db.session.query(
         StockPorTalle.talle,
         StockPorTalle.stock,
-        db.func.coalesce(Carrito.cantidad, 0).label("en_carrito")
-    ).outerjoin(Carrito, (StockPorTalle.producto_id == Carrito.producto_id) & (StockPorTalle.talle == Carrito.talle)) \
-     .filter(StockPorTalle.producto_id == producto.id).all()
+        func.coalesce(func.sum(Carrito.cantidad), 0).label("en_carrito")
+    )
+    .outerjoin(Carrito, (StockPorTalle.producto_id == Carrito.producto_id) & (StockPorTalle.talle == Carrito.talle))
+    .filter(StockPorTalle.producto_id == producto.id)
+    .group_by(StockPorTalle.talle, StockPorTalle.stock)
+    .all()
+)
 
-    talles = [
-        {'talle': t[0], 'stock': t[1], 'en_carrito': t[2]}
-        for t in raw_talles
-    ]
-
-    return render_template('producto_detalle.html', producto=producto, imagenes=imagenes, talles=talles)
-
+talles = [
+    {'talle': t[0], 'stock': t[1], 'en_carrito': t[2]}
+    for t in raw_talles
+]
 
 @app.route('/agregar_al_carrito/<slug>', methods=['POST'])
 def agregar_al_carrito(slug):
