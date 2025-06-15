b3a977b2194891fa399785d91c7890aec17dec85
MacbookPro <macbookpro@MacBook-Pro-de-MacbookPro.local>
2025-06-15 19:43:14 -0300

    arreglo precios y descripciones

--- init_db.py
+++ init_db.py
@@ -10,48 +10,48 @@ def cargar_datos():
         Producto(
             slug="miles-morales",
             nombre="Traje Miles Morales",
-            precio=45900,
-            descripcion="Inspirado en Spider-Man: Into the Spider-Verse",
+            precio=47900,
+            descripcion="Traje de SpiderMan de Miles Morales con diseño negro y rojo intenso inspirado en el estilo urbano y moderno de las peliculas animadas SpiderVerse. Incluye araña roja en el pecho patrones de telaraña estilizados y esta confeccionado en lycra elastica de alta calidad con estampado digital y cierre invisible Ideal para quienes buscan un look original juvenil y diferente dentro del universo SpiderMan. Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe Estado Nuevo sin uso en excelente estado Entrega inmediata Guia de talles en fotos. IMPORTANTE Este producto no es oficial ni licenciado por Marvel. Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="miles1_.webp",
             imagenes="miles5_.webp,miles1_.webp,miles2_.webp,miles3_.webp,miles4_.webp,miles0_.webp,talles.webp"
         ),
         Producto(
             slug="traje-mejorado",
-            nombre="Upgraded Suit",
-            precio=64900,
-            descripcion="Versión mejorada del traje clásico rojo y negro",
+            nombre="Traje Spider-man: Lejos De Casa",
+            precio=49000,
+            descripcion="Traje inspirado en SpiderMan ideal para jugar disfrazarse o eventos tematicos. Modelo Traje Mejorado Spiderman Lejos de casa. Completo mascara Material Lycra elastica sublimada Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe. Estado Nuevo sin uso en excelente estado Entrega inmediata Guia de talles en fotos. IMPORTANTE Este producto no es oficial ni licenciado por Marvel Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="mejorado0_.webp",
             imagenes="mejorado4_.webp,mejorado0_.webp,mejorado5_.webp,mejorado6_.webp,talles.webp"
         ),
         Producto(
             slug="electro",
             nombre="Traje Gold and Black",
-            precio=64900,
-            descripcion="Traje con detalles dorados, basado en No Way Home",
+            precio=57900,
+            descripcion="Traje inspirado en SpiderMan ideal para jugar disfrazarse o eventos tematicos. Modelo Traje negro y dorado Spiderman importado. Completo mascara Material Lycra elastica sublimada Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe. Estado Nuevo sin uso en excelente estado. Entrega inmediata Guia de talles en fotos. IMPORTANTE Este producto no es oficial ni licenciado por Marvel Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="electro0_.webp",
             imagenes="electro0_.webp,electro1_.webp,electro3_.webp,electro4_.webp,talles.webp"
         ),
         Producto(
             slug="spiderman-ps4",
             nombre="Traje Spider-Man PS4",
-            precio=64900,
-            descripcion="Traje del juego de PS4 con el logo blanco",
+            precio=57900,
+            descripcion="Traje inspirado en SpiderMan ideal para jugar disfrazarse o eventos tematicos. Modelo Traje PS4 Spiderman importado. Completo mascara Material Lycra elastica sublimada Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe. Estado Nuevo sin uso en excelente estado. Entrega inmediata. Guia de talles en fotos. IMPORTANTE Este producto no es oficial ni licenciado por Marvel Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="ps41_.webp",
             imagenes="ps40_.webp,ps41_.webp,talles.webp"
         ),
         Producto(
             slug="traje-clasico",
             nombre="Traje Clásico",
-            precio=64900,
-            descripcion="El traje tradicional rojo y azul",
+            precio=57900,
+            descripcion="Traje inspirado en SpiderMan ideal para jugar disfrazarse o eventos tematicos. Modelo Traje Clasico Spiderman Lejos de casa. Completo mascara Material Lycra elastica sublimada Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe. Estado Nuevo sin uso en excelente estado. Entrega inmediata Guia de talles en fotos IMPORTANTE. Este producto no es oficial ni licenciado por Marvel. Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="clasico9.webp",
             imagenes="clasico3.webp,clasico9.webp,clasico4.webp,clasico5.webp,talles.webp"
         ),
         Producto(
             slug="iron-spider",
             nombre="Traje Iron Spider",
-            precio=64900,
-            descripcion="Basado en Avengers: Infinity War",
+            precio=57900,
+            descripcion="Traje inspirado en SpiderMan ideal para jugar disfrazarse o eventos tematicos. Modelo Traje Iron Spider. Completo mascara Material Lycra elastica sublimada Ideal para fiestas Halloween cosplay o simplemente jugar como un heroe. Estado Nuevo sin uso en excelente estado. Entrega inmediata Guia de talles en fotos IMPORTANTE. Este producto no es oficial ni licenciado por Marvel. Es un disfraz inspirado de excelente calidad y muy comodo",
             imagen="iron-spider0_.webp",
             imagenes="iron-spider3_.webp,iron-spider1_.webp,iron-spider2_.webp,iron-spider0_.webp,iron-spider4_.webp,talles.webp"
         ),
